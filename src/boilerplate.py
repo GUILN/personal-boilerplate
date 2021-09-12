7b3af9c5e2cd0971e3dffd01fc19b894369366f7
guiln <guilherme.l.nunes@gmail.com>
2021-09-12 19:42:26 +0100

    get command working

--- src/boilerplate.py
+++ src/boilerplate.py
@@ -34,28 +34,19 @@ def display_boilerplates_list():
     root_command = get_root_command()    # Printing all commands
     print(root_command.print_all_commands())
 
-def get_boilerplate(boilerplate_path: list):
+def get_boilerplate(boilerplate_path: str):
+    boilerplate_path = f'*-{boilerplate_path}'
     root_command = get_root_command()
-    path = root_command.get_path(boilerplate_path)
+    path = root_command.get_path(boilerplate_path.split('-'))
     print(path)
 
 def get_root_command() -> Command:
     read_data = get_from_autoconf_file()
     return get_config_root_command(read_data)
 
-
-def run_program():
-    if UPDATE_FROM_REPO:
-        update_autoconf_file()
-
-    #command_to_get = input('type the command you want \n')
-    #path = root_command.get_path(command_to_get.split())
-
-    #print(path)
-
 class Boilerplate(object):
-    def get(self, *boilerplate_path):
-        get_boilerplate(list(boilerplate_path))
+    def get(self, path_separated_by_dash):
+        get_boilerplate(path_separated_by_dash)
 
     def update(self):
         update_autoconf_file()
