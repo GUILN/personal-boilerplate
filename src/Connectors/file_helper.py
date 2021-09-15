63f14f3c3baa86ff212fee4dd735928e27c08821
guiln <guilherme.l.nunes@gmail.com>
2021-09-15 23:16:21 +0100

    Some tests on how to create the folder

--- src/Connectors/file_helper.py
+++ src/Connectors/file_helper.py
@@ -1,5 +1,12 @@
-
+import os 
 
 def write_out_file(file_name: str, file_content: str):
     with open(file_name, "w") as file_out:
        file_out.write(file_content) 
+
+def get_relative_path(actual_path: str, common_path:str) -> str: 
+    print(f'actual path {os.path.dirname(actual_path)}')
+    print(f'common path {os.path.dirname(common_path)}')
+    #relative_path = os.path.relpath(os.path.abspath(actual_path), start=os.path.abspath(common_path))
+    relative_path = actual_path.replace(common_path, '')
+    return relative_path 
--- src/Connectors/github_connector.py
+++ src/Connectors/github_connector.py
@@ -2,7 +2,7 @@ import os
 import base64
 from github import Github, Repository
 from github.GithubException import GithubException
-from .file_helper import write_out_file
+from .file_helper import write_out_file, get_relative_path
 
 
 class GithubConnector:
@@ -18,22 +18,23 @@ class GithubConnector:
 
     def download_boilerplate(self, path_to_boilerplate: str):
         contents = self.boilerplate_repository.get_contents(path_to_boilerplate)
-
+        print(f'This is the parameter path: {path_to_boilerplate}')
         for content in contents:
-            print(f'Downloading {content}...')
             if content.type == 'dir':
-                self.download_boilerplate(content.path)
+
+                os.makedirs(content.path)
+                print(f'This is a directory {content.path}')
+                self.download_boilerplate(content.path) 
             else:
                 try:
-                    print(f'Downloading file {content.name}')
-                    print(f'path is {content.path}')
-                    file_content = self.boilerplate_repository.get_contents(content.path)
+                    print(f'Downloading file {content.path}') 
+                    file_content = self.boilerplate_repository.get_contents(content.path) 
                     file_data = base64.b64decode(file_content.content)
                     write_out_file(content.name, file_data.decode())
                 except (GithubException, IOError) as e:
-                    raise(Exception(f'Error in download_boilerplate when processing: {content.path} - {e}'))
+                    raise(Exception(f'Error in download_boilerplate when processing: {content.path} - {e}')) 
 
-    def __traverse_repo(self, repo, dir_name: str, root_name: str = "*") -> dict:
+    def __traverse_repo(self, repo, dir_name: str, root_name: str = "*") -> dict: 
         file_contents = repo.get_contents(dir_name)
         is_boilerplate = True if len([c for c in file_contents if c.name == '.boilerplate']) > 0 else False
         root_dict = {}
@@ -46,6 +47,6 @@ class GithubConnector:
             inner_dirs = [c for c in file_contents if c.type == 'dir']
             for dir in inner_dirs:
                 new_dir_to_traverse = dir_name + f'/{dir.name}'
-                root_dict[root_name].append(self.__traverse_repo(repo, new_dir_to_traverse, dir.name))
+                root_dict[root_name].append(self.__traverse_repo(repo, new_dir_to_traverse, dir.name)) 
 
         return root_dict
